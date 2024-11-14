cd688381efc9dd12acc28a6102c01e9a916243f0
Albert Salud <albertsalud@gmail.com>
2024-11-14 22:57:07 +0100

    initial import

---
+++ crud-library/src/test/java/com/crud_library/CrudLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
